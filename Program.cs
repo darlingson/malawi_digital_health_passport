f1544a4f5233760f6a8db4cf01ddd7b649cfe0b9
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-15 18:37:00 +0200

    Started implementing user class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using malawi_digital_health_passport;
+
 Console.WriteLine("Hello, World!");
+User user = new User("Darlingson","admin1","123456789","dmakuwila");
+Console.WriteLine("name :" +  user.Name);
--- User.cs
+++ User.cs
@@ -8,6 +8,16 @@ namespace malawi_digital_health_passport;
 
     public class User
     {
+    public User()
+    {
+    }
+
+    public User(string name,string user_id,string password,string email) {
+        Name = name;
+        User_id = user_id;
+        Password = password;
+        Email = email;
+    }
         public string Name { get; set; }
         public string User_id { get; set; }
         public string Password { get; set; }
