c5f0fa26fab6f479f5cde0971fdc8a0660815b0b
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-24 13:14:59 +0200

    added Diagnosis.cs and updated Patient.cd

---
+++ Diagnosis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace malawi_digital_health_passport
+{
+    class Diagnosis
+    {
+        public Diagnosis(string date, string patientID) {
+            Date = date;
+            PatientID = patientID;
+        }
+        private string Date { get; set; }
+        private string PatientID { get; set; }
+        private string Lab { get; set;}
+        private string Treatment {  get; set; }
+        private string Notes { get; set; }
+    }
+}
---
+++ Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace malawi_digital_health_passport
+{
+    class Patient
+    {
+        public Patient() { }
+        private string patientName{get;set;}
+        private string dob { get;set;}
+        private string patientId { get;set;}
+        private string location {  get;set;}
+        private char sex { get;set;}
+        private string city { get;set;}
+        private string country { get;set;}
+        private string TA { get;set;}
+        private string village { get;set;}
+    }
+}
