dfcd9ba186ad4a78c799f6a8bddbaa9dd9ac214f
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-10-14 13:52:33 +0200

    updated

--- Patient.cs
+++ Patient.cs
@@ -8,15 +8,91 @@ namespace malawi_digital_health_passport
 {
     class Patient
     {
+        private string patientName;
+        private string patientId;
+        private string dob;
+        private string location;
+        private string sex;
+        private string city;
+        private string country;
+        private string TA;
+        private string village;
         public Patient() { }
-        private string patientName{get;set;}
-        private string dob { get;set;}
-        private string patientId { get;set;}
-        private string location {  get;set;}
-        private char sex { get;set;}
-        private string city { get;set;}
-        private string country { get;set;}
-        private string TA { get;set;}
-        private string village { get;set;}
+
+        //getter methods
+        public string getPatientName()
+        {
+            return patientName;
+        }
+        public string getPatientID()
+        {
+            return patientName;
+        }
+        public string getPatientDOB()
+        {
+            return patientName;
+        }
+        public string getPatientLocation()
+        {
+            return patientName;
+        }
+        public string getPatientSex()
+        {
+            return patientName;
+        }
+        public string getPatientCity()
+        {
+            return patientName;
+        }
+        public string getPatientCountry()
+        {
+            return patientName;
+        }
+        public string getPatientTA()
+        {
+            return patientName;
+        }
+        public string getPatientVillage()
+        {
+            return patientName;
+        }
+
+        //setter methods
+        public void setPatientName(string name)
+        {
+            this.patientName = name ;
+        }
+        public void setPatientID(string ID)
+        {
+            this.patientId = ID;
+        }
+        public void setPatientDOB(string DOB)
+        {
+            this.dob = DOB;
+        }
+        public void setPatientLocation(string Location)
+        {
+            this.location = Location;
+        }
+        public void setPatientSex(string Sex)
+        {
+            this.sex = Sex;
+        }
+        public void setPatientCity(string City)
+        {
+            this.city = City;
+        }
+        public void setPatientCountry(string Country)
+        {
+            this.country = Country;
+        }
+        public void setPatientTA(string TA)
+        {
+            this.TA = TA;
+        }
+        public void setPatientVillage(string Village)
+        {
+            village = Village;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,3 +4,6 @@ using malawi_digital_health_passport;
 Console.WriteLine("Hello, World!");
 User user = new User("Darlingson","admin1","123456789","dmakuwila");
 Console.WriteLine("name :" +  user.Name);
+//create a patient
+Patient patient = new Patient();
+patient.setPatientName = "Darlingson";
